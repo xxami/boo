e3cecf85f41a8bf8849ce7a3ed8905f487b46f39
lilah <lilah.willacott@gmail.com>
2017-06-13 16:21:37 +0100

    add idler setup

--- boo.js
+++ boo.js
@@ -3,15 +3,51 @@ const dotenv = require('dotenv');
 const discord = require('discord.js');
 const client = new discord.Client();
 
+class Idler {
+	
+	constructor() {
+		this.players = {};
+	}
+	
+	addPlayer(id, username) {
+		this.players[id] = username;
+	}
+	
+	hasPlayer(id) {
+		if (this.players[id] === undefined) {
+			return false;
+		}
+		return true;
+	}
+	
+}
+
+const idler = new Idler();
+
 client.on('ready', () => {
 	console.log('I am ready!');
+	client.user.setGame("PokemonIdle: .help");
 });
 
 client.on('message', message => {
-	if (message.content === 'ping') {
-		message.reply('boo');
+	
+	if (message.content === '.help') {
+		message.reply('collect pokemon while you idle, type .register to get started!');
 	}
+	
+	else if (message.content === '.register') {
+		let user = message.client.user;
+		if (idler.hasPlayer(user.id)) {
+			message.reply('you are already a registered player!');
+		}
+		else {
+			idler.addPlayer(user.id, user.username);
+			message.reply('you are now playing PokemonIdle, good luck!');
+		}
+	}
+	
 });
 
 dotenv.config({path: 'boo.env'});
 client.login(process.env.BOO_TOKEN);
+
