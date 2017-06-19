c70666d403002789511d069c88f0358bf075c028
lilah <lilah.willacott@gmail.com>
2017-06-19 21:05:14 +0100

    add sample tcg data set

---
+++ idletcg/tcgdata.js
@@ -0,0 +1,18 @@
+
+const boosters = [
+	'January',
+	'February'
+];
+
+const boosterDescript = 'Booster Pack';
+
+const cards = {
+	'January': [
+		{'img': 'res/test_a.png', 'text': 'Test A (Jan)'},
+		{'img': 'res/test_b.png', 'text': 'Test B (Jan)'}
+	],
+	'February': [
+		{'img': 'res/test_a.png', 'text': 'Test A (Feb)'},
+		{'img': 'res/test_b.png', 'text': 'Test B (Feb)'}
+	]
+};
