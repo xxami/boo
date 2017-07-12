7cacf00af180616819bc003e8ad5c549b20b7557
lilah <lilah.willacott@gmail.com>
2017-07-12 10:28:17 +0100

    add http client template

---
+++ http-client/src/boo.js
@@ -0,0 +1,8 @@
+
+import Vue from 'vue'
+import boo from './boo.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(boo)
+});
---
+++ http-client/src/boo.vue
@@ -0,0 +1,34 @@
+
+<template>
+
+  <div id="app">
+    <article class="box" v-for="post in posts">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.body }}</p>
+    </article>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  
+  name: 'app',
+  
+  data () {
+    return {
+      posts: [
+        {title: 'First post', body: 'Lorem ipsum dolor sit amet'},
+        {title: 'Second post', body: 'Lorem ipsum dolor sit amet'}
+      ]
+    }
+  }
+
+}
+
+</script>
+
+<style>
+
+</style>
