00a57333bab4503db38448b500a834dc1c7243de
lilah <lilah.willacott@gmail.com>
2017-07-13 16:27:38 +0100

    add view templates for collection

--- http-server/boo.js
+++ http-server/boo.js
@@ -3,17 +3,26 @@ const koa = require('koa');
 const loggerware = require('koa-logger');
 const bodyware = require('koa-body');
 const serverware = require('koa-static');
+const routerware = require('koa-router')();
+const render = require('./lib/render');
 
 const app = module.exports = new koa();
 
-// "database"
-const posts = [];
+routerware.get('/collection/:id', viewCollection);
 
 // middleware
 app.use(loggerware());
 app.use(serverware(__dirname + '/public'));
+app.use(render);
 app.use(bodyware());
+app.use(routerware.routes());
+
+async function viewCollection(ctx) {
+  const id = ctx.params.id;
+  await ctx.render('collection');
+}
 
 if (!module.parent) app.listen(3000, () => {
   console.log('Server listening at http://localhost:3000/');
 });
+
---
+++ http-server/lib/render.js
@@ -0,0 +1,7 @@
+
+const views = require('koa-views');
+const path = require('path');
+
+module.exports = views(path.join(__dirname, '/../views'), {
+  map: { html: 'swig' }
+});
