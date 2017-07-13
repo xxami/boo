c41c5bdbb44f7dee75779a6fef48d845099534f1
lilah <lilah.willacott@gmail.com>
2017-07-13 19:36:03 +0100

    return playerdata in collection route

--- http-server/boo.js
+++ http-server/boo.js
@@ -4,13 +4,14 @@ const loggerware = require('koa-logger');
 const bodyware = require('koa-body');
 const serverware = require('koa-static');
 const routerware = require('koa-router')();
+const storage = require('node-persist');
 const render = require('./lib/render');
 
 const app = module.exports = new koa();
 
+storage.initSync({dir: '../discord-bot/.node-persist/storage'});
 routerware.get('/collection/:id', viewCollection);
 
-// middleware
 app.use(loggerware());
 app.use(serverware(__dirname + '/public'));
 app.use(render);
@@ -18,8 +19,20 @@ app.use(bodyware());
 app.use(routerware.routes());
 
 async function viewCollection(ctx) {
-  const id = ctx.params.id;
-  await ctx.render('collection');
+  let id = ctx.params.id;
+
+  let players = storage.getItemSync('players');
+  if (players === undefined) {
+    return;
+  }
+
+  let player = players[id];
+  if (player === undefined) {
+    return;
+  }
+
+  let playerData = JSON.stringify(player);
+  await ctx.render('collection', {playerData: playerData});
 }
 
 if (!module.parent) app.listen(3000, () => {
