d67e2e447a10d06f91567338c697cb5213ccd011
lilah <lilah.willacott@gmail.com>
2017-07-15 21:41:41 +0100

    add tcgdata to server template

---
+++ http-server/public/tcgdata.js
@@ -0,0 +1,24 @@
+
+const boosters = [
+	'January',
+	'February'
+];
+
+const boosterDescript = 'booster box';
+
+const cards = {
+	'January': [
+		{'img': 'res/test_a.png', 'id': 1, 'text': 'Test A (Jan)', 'description': 'A card'},
+		{'img': 'res/test_b.png', 'id': 2, 'text': 'Test B (Jan)', 'description': 'B card'}
+	],
+	'February': [
+		{'img': 'res/test_a.png', 'id': 3, 'text': 'Test A (Feb)', 'description': 'A card'},
+		{'img': 'res/test_b.png', 'id': 4, 'text': 'Test B (Feb)', 'description': 'B card'}
+	]
+};
+
+const titles = {
+	0: 'Newbie',
+	1: 'Potato',
+	4: 'Neko'
+}
